092b26e4d30a265667ea6d812146dd9ace903da8
pmcp <maartenlauwaert@gmail.com>
2019-11-06 17:50:27 +0100

    started with gridsome

---
+++ src/.temp/now.js
@@ -0,0 +1,1 @@
+export default 1573059000678
---
+++ src/.temp/routes.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    path: "/about/",
+    component: () => import(/* webpackChunkName: "page--src--pages--about-vue" */ "/Users/pmcp/dev/aroma/src/pages/About.vue")
+  },
+  {
+    name: "404",
+    path: "/404/",
+    component: () => import(/* webpackChunkName: "page--node-modules--gridsome--app--pages--404-vue" */ "/Users/pmcp/dev/aroma/node_modules/gridsome/app/pages/404.vue")
+  },
+  {
+    name: "home",
+    path: "/",
+    component: () => import(/* webpackChunkName: "page--src--pages--index-vue" */ "/Users/pmcp/dev/aroma/src/pages/Index.vue")
+  },
+  {
+    name: "*",
+    path: "*",
+    component: () => import(/* webpackChunkName: "page--node-modules--gridsome--app--pages--404-vue" */ "/Users/pmcp/dev/aroma/node_modules/gridsome/app/pages/404.vue")
+  }
+]
+
