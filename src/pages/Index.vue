50e738de4ca5d7b6a04efc9efdff05a8612bcc8b
pmcp <maartenlauwaert@gmail.com>
2019-11-26 13:49:15 +0100

    fixed the link grapql launch content

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout>
-    <aroma-launchpage :content="$page"></aroma-launchpage>
+    <aroma-launchpage :content="$page.content.content"></aroma-launchpage>
   </Layout>
 </template>
 
