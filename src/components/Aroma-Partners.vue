893e9b6eddbdfd3c372ade155184ebe5c7dc4c20
pmcp <maartenlauwaert@gmail.com>
2019-11-27 11:18:54 +0100

    added band of logos

--- src/components/Aroma-Partners.vue
+++ src/components/Aroma-Partners.vue
@@ -1,13 +1,21 @@
 <template>
-  <div class="parent grid grid-gap-md padding-y-xl justify-center">
-    <div v-for="partner in $static.partners.edges" v-bind:key="partner.id" class="col-2" >
+  <div class="parent grid  padding-y-xl justify-center">
+    
+    <div  class="col-12" >
+       <g-image
+              src="~/assets/img/logos.jpg"
+              
+            />
+        
+    </div>
+    <!-- <div v-for="partner in $static.partners.edges" v-bind:key="partner.id" class="col-2" >
       
       
       <a v-if="partner.node.content.url" :href="partner.node.content.url.url" target="_blank" :title="partner.node.name">
         <g-image :src="partner.node.content.logo" :alt="partner.node.name" width="500"/>
       </a>
       <g-image v-else :src="partner.node.content.logo" :alt="partner.node.name" width="500"/>
-    </div>
+    </div> -->
   </div>
 </template>
 
