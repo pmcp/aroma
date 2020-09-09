0419c77e64fa801a456b3b9a4dee7d3a337d4f7b
PMCP <maartenlauwaert@gmail.com>
2020-09-09 13:29:50 +0200

    changed articles to all articles automatically

--- src/components/Aroma-PageArticles.vue
+++ src/components/Aroma-PageArticles.vue
@@ -10,45 +10,68 @@
       <template v-slot:title>{{ content.title }}</template>
       <template v-slot:subtitle>{{ content.subtitle }}</template>
       <template v-slot:subtext>{{ content.subtext }}</template>
-    </aroma-hero>  
+    </aroma-hero>
+    
     <div class="container max-width-adaptive-lg padding-top-xl">
-      
       <div class="grid grid-gap-lg">
-          <aroma-preview-article
-            :article="content.articles[0]"
-            size="xl"
-            class="col-4@md article-list"
-          >
-          </aroma-preview-article>
+        <aroma-preview-article
+          :article="content.articles[0]"
+          size="xl"
+          class="col-4@md article-list"
+        >
+        </aroma-preview-article>
+<!-- {{ $static.allStoryblokEntry.edges }} -->
+<!-- <div v-for="article in $static.allStoryblokEntry.edges">
+  {{ article.node }}
+</div> -->
 
-          <aroma-preview-article
-            v-for="article in content.articles"
-            :key="article.uuid"
-            :article="article"
-            :lang="lang"
-            class="col-4@md article-list"
-
-
-          >
-          </aroma-preview-article>
-        </div>
-
+        <aroma-preview-article
+          v-for="article in articles"
+          :key="article.node.uuid"
+          :article="article.node"
+          :lang="lang"
+          class="col-4@md article-list"
+        >
+        </aroma-preview-article>
       </div>
     </div>
   </div>
 </template>
+
+<static-query>
+  query {
+    allStoryblokEntry(filter: { full_slug: { regex: "articles" }}) {
+        edges {
+          node {
+            lang
+            id
+            uuid
+            full_slug
+            name
+            content
+          }
+        }
+      }
+  }
+</static-query>
+
 <script>
 export default {
   props: {
     content: {
       type: Object,
-      default: () => ({})
+      default: () => ({}),
     },
     lang: {
       type: String,
-      default: 'default'
+      default: "default",
     },
-  }
+  },
+  computed: {
+    articles() {
+      return this.$static.allStoryblokEntry.edges.filter(article => article.node.lang === this.lang)
+    }
+  },
 };
 </script>
 
